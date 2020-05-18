d22606b687821a59f0df9b553a8b3a07f306d476
Chee Lam <CL LIM>
2020-05-18 16:22:06 +0800

    Updates

---
+++ precache-manifest.b0e3332124b3683552a6375e82c00846.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01349c232d6350912f94d00b247626e8",
+    "url": "/robofriend/index.html"
+  },
+  {
+    "revision": "cbcb14e194d6425e5a44",
+    "url": "/robofriend/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "10fb66a80ba0ef2be2b1",
+    "url": "/robofriend/static/css/main.cc3115b5.chunk.css"
+  },
+  {
+    "revision": "cbcb14e194d6425e5a44",
+    "url": "/robofriend/static/js/2.800020c3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriend/static/js/2.800020c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10fb66a80ba0ef2be2b1",
+    "url": "/robofriend/static/js/main.8cefd280.chunk.js"
+  },
+  {
+    "revision": "304188c1743133897544",
+    "url": "/robofriend/static/js/runtime-main.8ce40bb9.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriend/static/media/SEGA.12a3db8b.woff"
+  }
+]);
